2ad1dfdf0dfb9c0d39c1bf787d8bcd563717f3de
이경민 <leegm218@naver.com>
2021-11-07 21:57:06 +0900

    Rotate Logo Image #1

--- blogapp/templatetags/static.py
+++ blogapp/templatetags/static.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 class CustomStaticNode(StaticNode):
     def url(self, context):
-        version = 2021_10_10
+        version = 2021_11_07
         path = f'{super().url(context)}?v={version}'
         return path
 
