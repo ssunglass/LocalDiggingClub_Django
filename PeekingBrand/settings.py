a7512fc3dee0694550115bd714b6cf1eb8fabccd
이경민 <leegm218@naver.com>
2021-11-23 01:48:09 +0900

    Change local db to RdS Mysql #5

--- PeekingBrand/settings.py
+++ PeekingBrand/settings.py
@@ -93,6 +93,10 @@ DATABASES = {
         'PASSWORD': 'dangtangg9122005',
         'HOST': 'localdiggingclubdb.cahodehbnfjb.ap-northeast-2.rds.amazonaws.com',
         'PORT': '3306',
+        'OPTIONS': {
+           'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+
+        },
 
     }
 }
